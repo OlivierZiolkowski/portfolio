4cbc1dbe4072ad0a51f818d6370b13e99d308863
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2022-01-18 12:46:37 +0100

    Update homepage OpenGraph meta infos

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,8 @@ import Header from "../components/Header";
 import Technologies from "../components/Technologies";
 import Experiences from "../components/Experiences";
 import Projects from "../components/Projects";
+import Head from "@docusaurus/Head";
+import avatar from "../../static/img/avatar.png";
 
 export default function Home() {
   return (
@@ -11,6 +13,19 @@ export default function Home() {
       title={`Olivier Ziolkowski, développeur web front-end`}
       description="Bienvenue sur mon portfolio qui comprend un résumé de mes projets, mes expériences ainsi qu'un blog sur des sujets qui me passe par la tête."
     >
+      <Head>
+        <meta
+          property="og:title"
+          content="Olivier Ziolkowski, développeur web front-end"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://olivierziol.fr" />
+        <meta property="og:image" content={avatar} />
+        <meta
+          property="og:description"
+          content="Bienvenue sur mon portfolio qui comprend un résumé de mes projets, mes expériences ainsi qu'un blog sur des sujets qui me passe par la tête."
+        />
+      </Head>
       <div className="container">
         <Header />
         <main>
