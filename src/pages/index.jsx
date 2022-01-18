0f3d526eba5986f485e75dfb3e3dc864b4ada787
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2022-01-18 15:53:39 +0100

    Adding meta image for homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,18 @@ import Header from "../components/Header";
 import Technologies from "../components/Technologies";
 import Experiences from "../components/Experiences";
 import Projects from "../components/Projects";
+import Head from "@docusaurus/Head";
+import metaImage from "../../static/img/metaImage.jpg";
 
 export default function Home() {
   return (
     <Layout
-      title={`Olivier Ziolkowski, développeur web front-end`}
+      title={`Accueil`}
       description="Bienvenue sur mon portfolio qui comprend un résumé de mes projets, mes expériences ainsi qu'un blog sur des sujets qui me passe par la tête."
     >
+      <Head>
+        <meta property="og:image" content={metaImage} />
+      </Head>
       <div className="container">
         <Header />
         <main>
